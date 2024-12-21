17f19be36585547d89a66d693e12dd1f679a1ff3
unknown <irodrigomonzon@gmail.com>
2024-12-20 21:14:08 -0300

    finalizando el proyecto

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Usuario
+from .models import Usuario, InfoUsuario
 
 class UsuarioAdmin(admin.ModelAdmin):
     list_display = ('nombre', 'email', 'edad', 'fecha_creacion')
@@ -7,3 +7,4 @@ class UsuarioAdmin(admin.ModelAdmin):
     list_filter = ('edad',)
 
 admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(InfoUsuario)
